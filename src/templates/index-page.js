0f7c2da4878799397938d071554075b33d45c626
Matthew Fisher <m.r.f.48215@gmail.com>
2019-03-12 15:49:21 +0900

    more changes!

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -35,8 +35,8 @@ export const IndexPageTemplate = ({
       <h1
         className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
         style={{
-          boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-          backgroundColor: 'rgb(255, 68, 0)',
+          boxShadow: '#6BB6B6 0.5rem 0px 0px, #6BB6B6 -0.5rem 0px 0px',
+          backgroundColor: '6BB6B6',
           color: 'white',
           lineHeight: '1',
           padding: '0.25em'
