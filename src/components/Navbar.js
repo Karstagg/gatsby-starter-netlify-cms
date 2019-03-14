aaf0297c2dbc17305ff3bc99f67b8d2fc6105924
Matthew Fisher <m.r.f.48215@gmail.com>
2019-03-14 17:19:22 +0900

    background image change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <h1>Fuck yeah</h1>
+              <h1>Home</h1>
             </Link>
             {/* Hamburger menu */}
             <div
